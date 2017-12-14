56321882cea12cda16bf90dff8ec8758f77bede4
alejandra <dubrasskabreu@gmail.com>
2017-12-14 15:22:00 -0300

    FoodMap navegacion completa

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	//img de intro.
+	/*$('#principal').hide();
+   $('#splash').fadeOut(2000);
+   $('#principal').fadeIn();*/
+
+
+//------
+$("#steven1").hover(function(){ //hover para texto sobre las imágenes
+        $("#steven1").append("<span id='word-img'>Steven Universe</span>");
+        }, function(){
+            $(".word-img").remove();
+        });
+$("#gr").click(function(){
+	$("#gr").window.location.href = "file:///C:/Users/Alejandra/Desktop/responsive/retos/Food%20Map/greg.html";
+}
+
+});
+
+	/*img de intro.
+	$('.principal').hide();*/
+   $('#splash').fadeOut(500);
+   console.log()
+   /*$('.principal').fadeIn();*/
